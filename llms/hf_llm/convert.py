d097652adc50eb6e6b5b915733bd39aadc30085d
Awni Hannun <awni@apple.com>
2024-01-03 13:59:28 -0800

    model card

--- llms/hf_llm/convert.py
+++ llms/hf_llm/convert.py
@@ -71,20 +71,36 @@ def make_shards(weights: dict, max_file_size_gibibyte: int = 15):
 
 
 def upload_to_hub(path: str, name: str):
-    from huggingface_hub import HfApi, logging
+    import os
+
+    from huggingface_hub import HfApi, ModelCard, logging
+
+    repo_id = f"mlx-community/{name}"
+
+    card = ModelCard.load(hf_path)
+    card.data.tags = ["mlx"] if card.data.tags is None else card.data.tags + ["mlx"]
+    card.text = f"""
+# {name}
+This model was converted to MLX format from [`{hf_path}`]().
+Refer to the [original model card](https://huggingface.co/{hf_path}) for more details on the model.
+## Use with mlx
+```bash
+pip install mlx
+git clone https://github.com/ml-explore/mlx-examples.git
+cd mlx-examples/llms/hf_llm
+python generate.py --model {repo_id} --prompt "My name is"
+```
+"""
+    card.save(os.path.join(path, "README.md"))
 
     logging.set_verbosity_info()
 
     api = HfApi()
-
-    repo_id = f"mlx-community/{name}"
     api.create_repo(repo_id=repo_id, exist_ok=True)
     api.upload_folder(
         folder_path=path,
         repo_id=repo_id,
         repo_type="model",
-        multi_commits=True,
-        multi_commits_verbose=True,
     )
 
 
