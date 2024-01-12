98c5300ecbe01f3b8c68ca4d53f0c7a0ac17b932
Awni Hannun <awni@apple.com>
2024-01-12 09:45:20 -0800

    fix url

--- llms/setup.py
+++ llms/setup.py
@@ -8,14 +8,14 @@ with open(Path(__file__).parent / "mlx_lm/requirements.txt") as fid:
     requirements = [str(r) for r in pkg_resources.parse_requirements(fid)]
 setup(
     name="mlx-lm",
-    version="0.0.1",
+    version="0.0.0.dev7",
     description="LLMs on Apple silicon with MLX and the Hugging Face Hub",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     readme="README.md",
     author_email="mlx@group.apple.com",
     author="MLX Contributors",
-    utl="https://github.com/ml-explore/mlx-examples",
+    url="https://github.com/ml-explore/mlx-examples",
     license="MIT",
     install_requires=requirements,
     packages=["mlx_lm", "mlx_lm.models"],
