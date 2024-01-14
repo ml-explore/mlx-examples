25404dd0008aafea2aebce68fafe70238ef2ff66
anchen <li.anchen.au@gmail.com>
2024-01-14 01:02:36 -0800

    chore: remove unnecessary try catch

--- llms/mlx_lm/tokenizer_utils.py
+++ llms/mlx_lm/tokenizer_utils.py
@@ -26,41 +26,36 @@ def copy_tokenizer_files(model_path: Path, mlx_path: Path, model_type: str) -> l
     Returns:
     - list: List of copied file paths.
 
-    Raises:
-    - Specific exceptions related to file operations.
     """
-    try:
-        copied_files = []
-        auto_map_present = False
+    copied_files = []
+    auto_map_present = False
 
-        tokenizer_files = [
-            SPECIAL_TOKENS_MAP_FILE,
-            ADDED_TOKENS_FILE,
-            TOKENIZER_CONFIG_FILE,
-            FULL_TOKENIZER_FILE,
-        ]
+    tokenizer_files = [
+        SPECIAL_TOKENS_MAP_FILE,
+        ADDED_TOKENS_FILE,
+        TOKENIZER_CONFIG_FILE,
+        FULL_TOKENIZER_FILE,
+    ]
 
-        for file_name in tokenizer_files:
-            file_path = model_path / file_name
-            if file_path.exists():
-                shutil.copy(file_path, mlx_path)
-                copied_files.append(file_path)
-                if file_name == TOKENIZER_CONFIG_FILE:
-                    with open(file_path, "r") as f:
-                        config_data = json.load(f)
-                        auto_map_present = "auto_map" in config_data
+    for file_name in tokenizer_files:
+        file_path = model_path / file_name
+        if file_path.exists():
+            shutil.copy(file_path, mlx_path)
+            copied_files.append(file_path)
+            if file_name == TOKENIZER_CONFIG_FILE:
+                with open(file_path, "r") as f:
+                    config_data = json.load(f)
+                    auto_map_present = "auto_map" in config_data
 
-        for file in model_path.glob("*.json"):
-            if RE_TOKENIZER_FILE.match(file.name):
-                shutil.copy(file, mlx_path)
-                copied_files.append(file)
+    for file in model_path.glob("*.json"):
+        if RE_TOKENIZER_FILE.match(file.name):
+            shutil.copy(file, mlx_path)
+            copied_files.append(file)
 
-        if model_type in VOCAB_FILE_MAPPING and auto_map_present:
-            vocab_pattern = VOCAB_FILE_MAPPING[model_type]
-            for vocab_file in model_path.glob(vocab_pattern):
-                shutil.copy(vocab_file, mlx_path)
-                copied_files.append(vocab_file)
+    if model_type in VOCAB_FILE_MAPPING and auto_map_present:
+        vocab_pattern = VOCAB_FILE_MAPPING[model_type]
+        for vocab_file in model_path.glob(vocab_pattern):
+            shutil.copy(vocab_file, mlx_path)
+            copied_files.append(vocab_file)
 
-        return copied_files
-    except FileNotFoundError as e:
-        raise FileNotFoundError(f"tokenizer fils not found: {e}")
+    return copied_files
