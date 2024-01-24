ac4b3d99a3414e0c1ef40a9e086e89e0aa785ac4
anchen <li.anchen.au@gmail.com>
2024-01-25 00:36:14 +1100

    chore: disable quant in to linear when de-quant enabled

--- llms/mlx_lm/tuner/lora.py
+++ llms/mlx_lm/tuner/lora.py
@@ -18,7 +18,7 @@ class LoRALinear(nn.Module):
         lora_lin.linear = linear
         return lora_lin
 
-    def to_linear(self):
+    def to_linear(self, de_quantize: bool = False):
         linear = self.linear
         bias = "bias" in linear
         weight = linear.weight
@@ -45,7 +45,7 @@ class LoRALinear(nn.Module):
         if bias:
             fused_linear.bias = linear.bias
 
-        if is_quantized:
+        if is_quantized and not de_quantize:
             fused_linear = nn.QuantizedLinear.from_linear(
                 fused_linear,
                 linear.group_size,
