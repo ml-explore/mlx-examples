761b6162447323f5630c60dd8cd8b5e08c01c7da
Muhtasham Oblokulov <muhtasham97@gmail.com>
2024-02-29 19:52:11 +0100

    Refactor starcoder2.py: Update model arguments and modules

--- llms/mlx_lm/models/starcoder2.py
+++ llms/mlx_lm/models/starcoder2.py
@@ -10,25 +10,27 @@ from .base import BaseModelArgs
 
 @dataclass
 class ModelArgs(BaseModelArgs):
-    dim: int
-    n_layers: int
-    head_dim: int
-    hidden_dim: int
-    n_heads: int
-    n_kv_heads: int
-    norm_eps: float
-    vocab_size: int
-    rope_theta: float = 10000
+    model_type: str
+    hidden_size: int
+    num_hidden_layers: int
+    intermediate_size: int
+    num_attention_heads: int
+    num_key_value_heads: int = None
+    max_position_embeddings: int = 16384
+    norm_eps: float = None
+    rms_norm_eps: float = 1e-5
+    vocab_size: int = 49152
+    rope_theta: float = 100000
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, dims: int, eps: float = 1e-5):
+    def __init__(self, hidden_sizes: int, eps: float = 1e-5):
         super().__init__()
-        self.weight = mx.ones((dims,))
+        self.weight = mx.ones((hidden_sizes,))
         self.eps = eps
 
     def _norm(self, x):
-        return x * mx.rsqrt(x.square().mean(-1, keepdims=True) + self.eps)
+        return x * mx.rsqrt(x.square().mean(-1, keephidden_sizes=True) + self.eps)
 
     def __call__(self, x):
         output = self._norm(x.astype(mx.float32)).astype(x.dtype)
@@ -40,18 +42,20 @@ class Attention(nn.Module):
         super().__init__()
         self.args = args
 
-        self.n_heads: int = args.n_heads
-        self.n_kv_heads: int = args.n_kv_heads
+        dim = args.hidden_size
+        self.n_heads = n_heads = args.num_attention_heads
+        self.n_kv_heads = n_kv_heads = args.num_key_value_heads
 
         self.repeats = self.n_heads // self.n_kv_heads
 
-        self.scale = self.args.head_dim**-0.5
+        head_dim = args.hidden_size // args.num_attention_heads
+        self.scale = head_dim**-0.5
 
-        self.wq = nn.Linear(args.dim, args.n_heads * args.head_dim, bias=False)
-        self.wk = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
-        self.wv = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
-        self.wo = nn.Linear(args.n_heads * args.head_dim, args.dim, bias=False)
-        self.rope = nn.RoPE(args.head_dim, traditional=True, base=args.rope_theta)
+        self.wq = nn.Linear(dim, n_heads * head_dim, bias=False)
+        self.wk = nn.Linear(dim, n_kv_heads * head_dim, bias=False)
+        self.wv = nn.Linear(dim, n_kv_heads * head_dim, bias=False)
+        self.wo = nn.Linear(n_heads * head_dim, dim, bias=False)
+        self.rope = nn.RoPE(head_dim, traditional=True, base=args.rope_theta)
 
     def __call__(
         self,
@@ -90,26 +94,25 @@ class Attention(nn.Module):
 
 
 class MLP(nn.Module):
-    def __init__(self, args: ModelArgs):
+    def __init__(self, dim, hidden_dim):
         super().__init__()
-
-        self.w1 = nn.Linear(args.dim, args.hidden_dim, bias=False)
-        self.w2 = nn.Linear(args.hidden_dim, args.dim, bias=False)
-        self.w3 = nn.Linear(args.dim, args.hidden_dim, bias=False)
+        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
+        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
 
     def __call__(self, x) -> mx.array:
-        return self.w2(nn.silu(self.w1(x)) * self.w3(x))
+        return self.w2(nn.gelu(self.w1(x)) * self.w3(x))
 
 
 class TransformerBlock(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
-        self.n_heads = args.n_heads
-        self.dim = args.dim
+        self.hidden_size = args.hidden_size
+        self.n_heads = args.num_attention_heads
         self.attention = Attention(args)
-        self.feed_forward = MLP(args=args)
-        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
-        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.mlp = MLP(args.hidden_size, args.intermediate_size)
+        self.attention_norm = RMSNorm(args.hidden_size, eps=args.norm_eps)
+        self.ffn_norm = RMSNorm(args.hidden_size, eps=args.norm_eps)
         self.args = args
 
     def __call__(
@@ -130,12 +133,14 @@ class Starcoder2Model(nn.Module):
         super().__init__()
         self.args = args
         self.vocab_size = args.vocab_size
-        self.n_layers = args.n_layers
+        self.num_hidden_layers = args.num_hidden_layers
         assert self.vocab_size > 0
-        self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
-        self.layers = [TransformerBlock(args=args) for _ in range(args.n_layers)]
-        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
-        self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
+        self.tok_embeddings = nn.Embedding(args.vocab_size, args.hidden_size)
+        self.layers = [
+            TransformerBlock(args=args) for _ in range(args.num_hidden_layers)
+        ]
+        self.norm = RMSNorm(args.hidden_size, eps=args.norm_eps)
+        self.output = nn.Linear(args.hidden_size, args.vocab_size, bias=False)
 
     def __call__(
         self,
