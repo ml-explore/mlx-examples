10da1a7980da2855505b4e2cff7c4f874a531a61
anchen <li.anchen.au@gmail.com>
2024-03-22 02:08:48 +1100

    chore: add test

--- llms/mlx_lm/utils.py
+++ llms/mlx_lm/utils.py
@@ -17,6 +17,8 @@ from huggingface_hub import snapshot_download
 from mlx.utils import tree_flatten
 from transformers import AutoConfig, AutoTokenizer, PreTrainedTokenizer
 
+from .sample_utils import top_p_filtering
+
 # Local imports
 from .tuner.utils import apply_lora_layers
 from .tuner.utils import dequantize as dequantize_model
@@ -143,28 +145,8 @@ def generate_step(
         if temp == 0:
             token = mx.argmax(logits, axis=-1)
         else:
-            # referenced implementation from https://github.com/huggingface/transformers/blob/main/src/transformers/generation/logits_process.py#L449-L460
             if top_p > 0 and top_p < 1.0:
-                if (
-                    logits.dtype == mx.bfloat16
-                ):  # workdaround for unable to load kernel contiguous_scan_inclusive_sum_bfloat16_bfloat16
-                    logits = logits.astype(mx.float32)
-
-                probs = mx.softmax(logits / temp, axis=-1)
-
-                # sort probs in ascending order
-                sorted_probs = mx.sort(probs, axis=-1)
-                sorted_indices = mx.argsort(probs, axis=-1)
-                cumulative_probs = mx.cumsum(sorted_probs, axis=-1)
-
-                # select tokens with cumulative probs below threshold
-                top_probs = mx.where(
-                    cumulative_probs > 1 - top_p,
-                    sorted_probs,
-                    mx.zeros_like(sorted_probs),
-                )
-                sorted_token = mx.random.categorical(mx.log(top_probs))
-                token = sorted_indices.squeeze(0)[sorted_token]
+                token = top_p_filtering(logits, top_p, temp)
             else:
                 token = mx.random.categorical(logits * (1 / temp))
 
---
+++ llms/tests/test_sample_utils.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest.mock import patch
+
+import mlx.core as mx
+from mlx_lm.sample_utils import top_p_filtering
+
+
+class TestLora(unittest.TestCase):
+    @patch("mlx.core.random.categorical")
+    def test_top_p_filtering(self, mock_categorical):
+        logits = mx.array([[1.0, 2.0, 3.0, 4.0]])
+        top_p = 0.3
+        temperature = 1.0
+        expected_token = mx.array([3])
+        mock_categorical.return_value = expected_token
+
+        token = top_p_filtering(logits, top_p, temperature)
+        expected_top_probs = mx.array([[0.0, 0.0, 0.0, 0.643914]])
+        assert mx.allclose(token, expected_token)
+        args, _ = mock_categorical.call_args
+        assert mx.allclose(args[0], mx.log(expected_top_probs))
+
+        logits = mx.array([[1.0, 2.0, 3.0, 4.0]])
+        top_p = 0.9
+        temperature = 1.0
+        expected_token = mx.array([3])
+        mock_categorical.return_value = expected_token
+
+        token = top_p_filtering(logits, top_p, temperature)
+        expected_top_probs = mx.array([[0.0, 0.0871443, 0.236883, 0.643914]])
+        assert mx.allclose(token, expected_token)
+        args, _ = mock_categorical.call_args
+        assert mx.allclose(args[0], mx.log(expected_top_probs))
+
+
+if __name__ == "__main__":
+    unittest.main()
