c55de6399cb57f783536312933af5a6118edb6d9
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2024-03-28 21:39:33 -0400

    m2m attention layer

---
+++ m2m_100/m2m_100.py
@@ -0,0 +1,146 @@
+# Copyright © 2023 Apple Inc.
+
+import argparse
+import glob
+import json
+import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional, Tuple
+
+import mlx.core as mx
+import mlx.nn as nn
+from mlx.utils import tree_unflatten
+from sentencepiece import SentencePieceProcessor
+
+@dataclass
+class ModelArgs:
+    vocab_size: int
+    max_position_embeddings: int
+    encoder_layers: int
+    encoder_ffn_dim: int
+    encoder_attention_heads: int
+    decoder_layers: int
+    decoder_ffn_dim: int
+    decoder_attention_heads: int
+    encoder_layerdrop: float
+    decoder_layerdrop: float
+    use_cache: bool
+    is_encoder_decoder: bool    
+    activation_function: str
+    d_model: int
+    dropout: float
+    attention_dropout: float
+    activation_dropout: float
+    init_std: float
+    decoder_start_token_id: int
+    scale_embedding: bool
+    pad_token_id: int
+    bos_token_id: int
+    eos_token_id: int
+
+class M2M100Attention(nn.Module):
+    def __init__(self,
+        embed_dim: int,
+        num_heads: int,
+        dropout: float = 0.0,
+        is_decoder: bool = False,
+        bias: bool = True,
+        is_causal: bool = False,
+        args: ModelArgs = None):
+
+        super().__init__()
+        self.args = args
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.dropout = dropout
+
+        self.head_dim = embed_dim // num_heads
+
+        self.scaling = self.head_dim**-0.5
+        self.is_decoder = is_decoder
+        self.is_causal = is_causal
+
+        self.k_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.v_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+    
+    def _shape(self, tensor: mx.array, seq_len: int, bsz: int):
+        return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
+
+    def __call__(self,
+        hidden_states: mx.array,
+        key_value_states: Optional[mx.array] = None,
+        past_key_value: Optional[Tuple[mx.array]] = None,
+        attention_mask: Optional[mx.array] = None,
+        layer_head_mask: Optional[mx.array] = None,
+        ):
+
+        is_cross_attention = key_value_states is not None
+        bsz, tgt_len, _ = hidden_states.shape
+
+        query_states = self.q_proj(hidden_states)
+
+        if (is_cross_attention and past_key_value is not None and past_key_value[0].shape[2] == key_value_states.shape[1]):
+            key_states = past_key_value[0]
+            value_states = past_key_value[1]
+        elif is_cross_attention:
+            key_states = self._shape(self.k_proj(key_value_states), -1, bsz)
+            value_states = self._shape(self.v_proj(key_value_states), -1, bsz)
+        elif past_key_value:
+            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
+            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
+            key_states = mx.concatenate([past_key_value[0], key_states], axis=2)
+            value_states = mx.concatenate([past_key_value[1], value_states], axis=2)
+        else:
+            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
+            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
+        
+        if self.is_decoder:
+            past_key_value = (key_states, value_states)
+    
+        proj_shape = (bsz * self.num_heads, tgt_len, self.head_dim)
+
+        query_states = self._shape(query_states, tgt_len, bsz).reshape(proj_shape)
+        key_states = key_states.reshape(proj_shape)
+        value_states = value_states.reshape(proj_shape)
+
+        src_len = key_states.shape[1]
+        attn_weights = query_states @ key_states.transpose(1, 2)
+
+        if attn_weights.size() != (bsz * self.num_heads, tgt_len, src_len):
+            raise ValueError(
+                f"Attention weights should be of size {(bsz * self.num_heads, tgt_len, src_len)}, but is"
+                f" {attn_weights.size()}"
+            )
+        
+        if attention_mask is not None:
+            if attention_mask.size() != (bsz, 1, tgt_len, src_len):
+                raise ValueError(
+                    f"Attention mask should be of size {(bsz, 1, tgt_len, src_len)}, but is {attention_mask.size()}"
+                )
+
+            attn_weights = attn_weights.reshape(bsz, self.num_heads, tgt_len, src_len) + attention_mask
+            attn_weights = attn_weights.reshape(bsz * self.num_heads, tgt_len, src_len)
+        
+        attn_weights = mx.softmax(attn_weights, axis=-1).astype(attn_weights.dtype)
+
+        attn_outputs = attn_weights @ value_states
+
+        attn_output = attn_outputs.reshape(bsz, self.num_heads, tgt_len, self.head_dim)
+        attn_output = attn_output.transpose(1, 2)
+
+        attn_output = attn_output.reshape(bsz, tgt_len, self.embed_dim)
+        attn_output = self.out_proj(attn_output)
+
+        return attn_output
+            
+
+
+
+
+
+
+
