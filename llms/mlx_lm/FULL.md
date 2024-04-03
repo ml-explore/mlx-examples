f25b714fa8c070adc1c98160157cb7e830f70bd0
Jioh L. Jung <ziozzang@gmail.com>
2024-04-03 16:00:31 +0900

    Support Full fine tune.

---
+++ llms/mlx_lm/FULL.md
@@ -0,0 +1,178 @@
+# Fine-Tuning with Full Fine-Tuning
+
+You can use the `mlx-lm` package to fine-tune an LLM with full fine-tuning for a target task. Full fine-tuning works with the following model families:
+
+- Mistral
+- Llama
+- Phi2
+- Mixtral
+- Qwen2
+- Gemma
+- OLMo
+
+## Contents
+
+* [Run](#Run)
+  * [Fine-tune](#Fine-tune)
+  * [Evaluate](#Evaluate)
+  * [Generate](#Generate)
+* [Data](#Data)
+* [Memory Issues](#Memory-Issues)
+
+## Run
+
+The main command is `mlx_lm.full`. To see a full list of command-line options run:
+
+```shell
+python -m mlx_lm.full --help
+```
+
+Note, in the following the `--model` argument can be any compatible Hugging
+Face repo or a local path to a converted model.
+
+You can also specify a YAML config with `-c`/`--config`. For more on the format see the
+[example YAML](examples/lora_config.yaml). For example:
+
+```shell
+python -m mlx_lm.full --config /path/to/config.yaml
+```
+
+If command-line flags are also used, they will override the corresponding
+values in the config.
+
+### Fine-tune
+
+To fine-tune a model use:
+
+```shell
+python -m mlx_lm.full \
+    --model <path_to_model> \
+    --train \
+    --data <path_to_data> \
+    --iters 600
+```
+
+The `--data` argument must specify a path to a `train.jsonl`, `valid.jsonl`
+when using `--train` and a path to a `test.jsonl` when using `--test`. For more
+details on the data format see the section on [Data](#Data).
+
+For example, to fine-tune a Mistral 7B you can use `--model
+mistralai/Mistral-7B-v0.1`.
+
+
+By default, the model weights are saved in model.npz. You can specify
+the output location with `--model-file`.
+
+You can resume fine-tuning with an existing model with
+`--model-file <path_to_model.npz>`.
+
+### Evaluate
+
+To compute test set perplexity use:
+
+```shell
+python -m mlx_lm.full \
+    --model-file <path_to_model.npz> \
+    --data <path_to_data> \
+    --test
+```
+
+### Generate
+
+For generation use `mlx_lm.generate`:
+
+```shell
+python -m mlx_lm.generate \
+    --model-file <path_to_model.npz> \
+    --prompt "<your_model_prompt>"
+```
+
+## Data
+
+The full fine-tuning command expects you to provide a dataset with `--data`.  The MLX
+Examples GitHub repo has an [example of the WikiSQL
+data](https://github.com/ml-explore/mlx-examples/tree/main/lora/data) in the
+correct format.
+
+For fine-tuning (`--train`), the data loader expects a `train.jsonl` and a
+`valid.jsonl` to be in the data directory. For evaluation (`--test`), the data
+loader expects a `test.jsonl` in the data directory. 
+
+Currently, `*.jsonl` files support three data formats: `chat`,
+`completions`, and `text`. Here are three examples of these formats:
+
+`chat`:
+  
+```jsonl
+{"messages": [
+  {"role": "system", "content": "You are a helpful assistant." },
+  {"role": "user", "content": "Hello."},
+  {"role": "assistant", "content": "How can I assistant you today."},
+]}
+```
+
+`completions`:
+  
+```jsonl
+{"prompt": "What is the capital of France?", "completion": "Paris."}
+```
+
+`text`:
+
+```jsonl
+{"text": "This is an example for the model."}
+```
+
+Note, the format is automatically determined by the dataset. Note also, keys in
+each line not expected by the loader will be ignored.
+
+For the `chat` and `completions` formats, Hugging Face [chat
+templates](https://huggingface.co/blog/chat-templates) are used. This applies
+the model's chat template by default. If the model does not have a chat
+template, then Hugging Face will use a default. For example, the final text in
+the `chat` example above with Hugging Face's default template becomes:
+
+```text
+<|im_start|>system
+You are a helpful assistant.<|im_end|>
+<|im_start|>user
+Hello.<|im_end|>
+<|im_start|>assistant
+How can I assistant you today.<|im_end|>
+```
+
+If you are unsure of the format to use, the `chat` or `completions` are good to
+start with. For custom requirements on the format of the dataset, use the
+`text` format to assemble the content yourself.
+
+## Memory Issues
+
+Fine-tuning a large model with LoRA requires a machine with a decent amount
+of memory. Here are some tips to reduce memory use should you need to do so:
+
+
+1. Try using a smaller batch size with `--batch-size`. The default is `4` so
+   setting this to `2` or `1` will reduce memory consumption. This may slow
+   things down a little, but will also reduce the memory use.
+
+
+2. Longer examples require more memory. If it makes sense for your data, one thing
+   you can do is break your examples into smaller
+   sequences when making the `{train, valid, test}.jsonl` files.
+
+3. Gradient checkpointing lets you trade-off memory use (less) for computation
+   (more) by recomputing instead of storing intermediate values needed by the
+   backward pass. You can use gradient checkpointing by passing the
+   `--grad-checkpoint` flag. Gradient checkpointing will be more helpful for
+   larger batch sizes or sequence lengths with smaller or quantized models.
+
+For example, Required memory is depends on model's parameter or batch size . :
+
+```
+python full.py \
+    --model mistralai/Mistral-7B-v0.1 \
+    --train \
+    --batch-size 1 \
+    --data wikisql
+```
+
---
+++ llms/mlx_lm/full.py
@@ -0,0 +1,222 @@
+import argparse
+import json
+import math
+import re
+import types
+from pathlib import Path
+
+import mlx.nn as nn
+import mlx.optimizers as optim
+import numpy as np
+import yaml
+from mlx.utils import tree_flatten
+
+from .tuner.datasets import load_dataset
+from .tuner.trainer import TrainingArgs, TrainingCallback, evaluate, train
+from .tuner.utils import build_schedule
+from .utils import load
+
+yaml_loader = yaml.SafeLoader
+yaml_loader.add_implicit_resolver(
+    "tag:yaml.org,2002:float",
+    re.compile(
+        """^(?:
+     [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+    |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+    |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+    |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+    |[-+]?\\.(?:inf|Inf|INF)
+    |\\.(?:nan|NaN|NAN))$""",
+        re.X,
+    ),
+    list("-+0123456789."),
+)
+
+
+CONFIG_DEFAULTS = {
+    "model": "mlx_model",
+    "train": False,
+    "data": "data/",
+    "seed": 0,
+    "batch_size": 4,
+    "iters": 1000,
+    "val_batches": 25,
+    "learning_rate": 1e-5,
+    "steps_per_report": 10,
+    "steps_per_eval": 200,
+    "model_file": "model.npz",
+    "save_every": 100,
+    "test": False,
+    "test_batches": 500,
+    "max_seq_length": 2048,
+    "lr_schedule": None,
+}
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(description="Full fine-tuning.")
+    parser.add_argument(
+        "--model",
+        help="The path to the local model directory or Hugging Face repo.",
+    )
+
+    # Training args
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help="Do training",
+    )
+    parser.add_argument(
+        "--data",
+        type=str,
+        help="Directory with {train, valid, test}.jsonl files",
+    )
+    parser.add_argument("--batch-size", type=int, help="Minibatch size.")
+    parser.add_argument("--iters", type=int, help="Iterations to train for.")
+    parser.add_argument(
+        "--val-batches",
+        type=int,
+        help="Number of validation batches, -1 uses the entire validation set.",
+    )
+    parser.add_argument("--learning-rate", type=float, help="Adam learning rate.")
+    parser.add_argument(
+        "--steps-per-report",
+        type=int,
+        help="Number of training steps between loss reporting.",
+    )
+    parser.add_argument(
+        "--steps-per-eval",
+        type=int,
+        help="Number of training steps between validations.",
+    )
+    parser.add_argument(
+        "--model-file",
+        type=str,
+        help="Save/load path for the trained model weights.",
+    )
+    parser.add_argument(
+        "--save-every",
+        type=int,
+        help="Save the model every N iterations.",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Evaluate on the test set after training",
+    )
+    parser.add_argument(
+        "--test-batches",
+        type=int,
+        help="Number of test set batches, -1 uses the entire test set.",
+    )
+    parser.add_argument(
+        "--max-seq-length",
+        type=int,
+        help="Maximum sequence length.",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        default=None,
+        help="A YAML configuration file with the training options",
+    )
+    parser.add_argument(
+        "--grad-checkpoint",
+        action="store_true",
+        help="Use gradient checkpointing to reduce memory use.",
+    )
+    parser.add_argument("--seed", type=int, default=0, help="The PRNG seed")
+    return parser
+
+
+def run(args, training_callback: TrainingCallback = None):
+    np.random.seed(args.seed)
+
+    print("Loading pretrained model")
+    model, tokenizer = load(args.model)
+
+    print("Loading datasets")
+    train_set, valid_set, test_set = load_dataset(args, tokenizer)
+
+    if args.train:
+        print("Training")
+        # init training args
+        training_args = TrainingArgs(
+            batch_size=args.batch_size,
+            iters=args.iters,
+            val_batches=args.val_batches,
+            steps_per_report=args.steps_per_report,
+            steps_per_eval=args.steps_per_eval,
+            steps_per_save=args.save_every,
+            max_seq_length=args.max_seq_length,
+            grad_checkpoint=args.grad_checkpoint,
+        )
+
+        model.train()
+        opt = optim.Adam(
+            learning_rate=(
+                build_schedule(args.lr_schedule)
+                if args.lr_schedule
+                else args.learning_rate
+            )
+        )
+        # Train model
+        train(
+            model=model,
+            tokenizer=tokenizer,
+            args=training_args,
+            optimizer=opt,
+            train_dataset=train_set,
+            val_dataset=valid_set,
+            training_callback=training_callback,
+        )
+
+        # Save the model weights
+        mx.save(training_args.model_file, model.state)
+        print(f"Saved final model weights to {training_args.model_file}.")
+
+    # Load the model weights if they exist
+    if Path(args.model_file).is_file():
+        model.update(mx.load(args.model_file))
+    else:
+        raise ValueError(
+            f"Model file {args.model_file} missing. "
+            "Use --train to learn and save the model.npz."
+        )
+
+    if args.test:
+        print("Testing")
+        model.eval()
+
+        test_loss = evaluate(
+            model=model,
+            dataset=test_set,
+            tokenizer=tokenizer,
+            batch_size=args.batch_size,
+            num_batches=args.test_batches,
+        )
+
+        test_ppl = math.exp(test_loss)
+
+        print(f"Test loss {test_loss:.3f}, Test ppl {test_ppl:.3f}.")
+
+
+if __name__ == "__main__":
+    parser = build_parser()
+    args = parser.parse_args()
+    config = args.config
+    args = vars(args)
+    if config:
+        print("Loading configuration file", config)
+        with open(config, "r") as file:
+            config = yaml.load(file, yaml_loader)
+        # Prefer parameters from command-line arguments
+        for k, v in config.items():
+            if not args.get(k, None):
+                args[k] = v
+
+    # Update defaults for unspecified parameters
+    for k, v in CONFIG_DEFAULTS.items():
+        if not args.get(k, None):
+            args[k] = v
+    run(types.SimpleNamespace(**args))
