7d3bdc922643e255d60fc63b8d8ca69a06888091
Chime Ogbuji <chimezie@gmail.com>
2024-04-20 10:35:34 -0400

    Add hf_dataset configuration for using HF hub-hosted datasets for (Q)LoRA training

--- llms/mlx_lm/tuner/datasets.py
+++ llms/mlx_lm/tuner/datasets.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-
+from typing import Iterable
 from transformers import PreTrainedTokenizer
 
 
@@ -83,12 +83,83 @@ def create_dataset(path: Path, tokenizer: PreTrainedTokenizer = None):
         )
 
 
+class TextHFDataset(Dataset):
+    """
+    A Huggingface dataset for a single blob of text per record
+    https://huggingface.co/docs/datasets/en/access
+    """
+    def __init__(self, hf_dataset, tokenizer, text_feature):
+        self._data = hf_dataset
+        self._tokenizer = tokenizer
+        self._text_feature = text_feature
+
+    def __getitem__(self, idx: int):
+        return self._data[idx][self._text_feature]
+
+
+class CompletionsHFDataset(CompletionsDataset):
+    """
+    Prompt/completion data from a Huggingface dataset
+    https://huggingface.co/docs/datasets/en/access
+    """
+    def __init__(self, hf_dataset: Iterable, tokenizer: PreTrainedTokenizer,
+                 prompt_feature: str = None,  completion_feature: str = None):
+        """
+
+        :param hf_dataset: Iterable HF dataset instance in a 'split' and/or configuration
+        :param prompt_feature: The HF dataset 'feature' or column name for the instruction input
+        :param completion_feature: The HF dataset 'feature' or column name for the instruction completion
+        """
+        self._data = hf_dataset
+        self._tokenizer = tokenizer
+        self._prompt_feature = prompt_feature
+        self._completion_feature = completion_feature
+
+    def __getitem__(self, idx: int):
+        data = self._data[idx]
+        text = self._tokenizer.apply_chat_template(
+            [
+                {"role": "user", "content": data[self._prompt_feature]},
+                {"role": "assistant", "content": data[self._completion_feature]},
+            ],
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        return text
+
+
 def load_dataset(args, tokenizer: PreTrainedTokenizer):
-    names = ("train", "valid", "test")
-    data_path = Path(args.data)
-    train, valid, test = [
-        create_dataset(data_path / f"{n}.jsonl", tokenizer) for n in names
-    ]
+    if args.hf_dataset:
+        from datasets import load_dataset
+        train_split = args.hf_dataset.get("train_split", "train[:80%]")
+        valid_split = args.hf_dataset.get("valid_split", "train[-10%:]")
+        test_split = args.hf_dataset.get("test_split")
+        train = load_dataset(args.hf_dataset.name, args.hf_dataset.get("configuration"), split=train_split)
+        valid = load_dataset(args.hf_dataset.name, args.hf_dataset.get("configuration"), split=valid_split)
+        test = load_dataset(args.hf_dataset.name,
+                            args.hf_dataset.get("configuration"), split=test_split) if args.test else None
+        text_feature = args.hf_dataset.text_feature
+        prompt_feature = args.hf_dataset.prompt_feature
+        completion_feature = args.hf_dataset.completion_feature
+        if (prompt_feature and prompt_feature in train.features and completion_feature and
+                completion_feature in train.features):
+            train = CompletionsHFDataset(train, tokenizer, prompt_feature, completion_feature)
+            valid = CompletionsHFDataset(valid, tokenizer, prompt_feature, completion_feature)
+            test = CompletionsHFDataset(test, tokenizer, prompt_feature, completion_feature) if args.test else None
+        elif text_feature and text_feature in train.features:
+            train = TextHFDataset(train, tokenizer, text_feature)
+            valid = TextHFDataset(valid, tokenizer, text_feature)
+            test = TextHFDataset(test, tokenizer, text_feature) if args.test else None
+        else:
+            raise ValueError("Need to specify either a prompt and completion feature or a text feature which are "
+                             "features of the specified HF dataset")
+
+    else:
+        names = ("train", "valid", "test")
+        data_path = Path(args.data)
+        train, valid, test = [
+            create_dataset(data_path / f"{n}.jsonl", tokenizer) for n in names
+        ]
     if args.train and len(train) == 0:
         raise ValueError(
             "Training set not found or empty. Must provide training set for fine-tuning."
@@ -99,6 +170,6 @@ def load_dataset(args, tokenizer: PreTrainedTokenizer):
         )
     if args.test and len(test) == 0:
         raise ValueError(
-            "Test set not found or empty. Must provide test set for evaluation."
+            "Test set not found or empty. Must provide test set for ev aluation."
         )
     return train, valid, test
