3514d4dafb24af2047b1ed382096431f56260668
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-04-25 11:13:10 +0200

    quick fix and clean up

--- llms/mlx_lm/models/minicpm.py
+++ llms/mlx_lm/models/minicpm.py
@@ -7,7 +7,6 @@ import numpy as np
 
 from .base import BaseModelArgs
 
-
 @dataclass
 class ModelArgs(BaseModelArgs):
     model_type: str
@@ -26,18 +25,6 @@ class ModelArgs(BaseModelArgs):
     rope_traditional: bool = False
     rope_scaling: Optional[Dict[str, Union[str, float]]] = None
     tie_word_embeddings: bool = False
-
-    def __post_init__(self):
-        if self.num_key_value_heads is None:
-            self.num_key_value_heads = self.num_attention_heads
-
-        if self.rope_scaling:
-            required_keys = {"factor", "type"}
-            if not all(key in self.rope_scaling for key in required_keys):
-                raise ValueError(f"rope_scaling must contain keys {required_keys}")
-
-            if self.rope_scaling["type"] != "linear":
-                raise ValueError("rope_scaling 'type' currently only supports 'linear'.")
 
 
 class MiniCPMRMSNorm(nn.Module):
@@ -67,57 +54,47 @@ class Attention(nn.Module):
         self.args = args
 
         self.hidden_size = args.hidden_size
-        self.num_heads = args.num_attention_heads
+        self.num_heads = n_heads = args.num_attention_heads
         self.rope_theta = args.rope_theta
         self.max_position_embeddings = args.max_position_embeddings
 
-        self.scaling_factor = 1.0  # Default scaling factor
+        self.head_dim = head_dim = args.hidden_size // n_heads
+        self.scale = head_dim**-0.5
 
-        # Check if rope_scaling is set and has the 'factor' key
-        if args.rope_scaling and 'factor' in args.rope_scaling:
-            self.scaling_factor = float(args.rope_scaling['factor'])
-
-        self.head_dim = self.hidden_size // self.num_heads
         self.num_key_value_heads = args.num_key_value_heads
         self.num_key_value_groups = self.num_heads // self.num_key_value_heads
-        self.scale = self.head_dim**-0.5
 
         self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=False)
         self.k_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
         self.v_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
         self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias=False)
 
+        rope_scale = (
+            1 / args.rope_scaling["factor"]
+            if args.rope_scaling is not None and args.rope_scaling["type"] == "linear"
+            else 1
+        )
+
         self.rope = nn.RoPE(
             dims=self.head_dim,
             traditional=args.rope_traditional,
             base=self.rope_theta,
-            scale=self.scaling_factor,
+            scale=rope_scale,
         )
-
-    def compute_dynamic_base(self, N):
-        # Dynamically adjust the base if the sequence length exceeds the predefined max
-        if N > self.max_position_embeddings:
-            return self.rope_theta * ((self.scaling_factor * N / self.max_position_embeddings) - (self.scaling_factor - 1)) ** (self.dims / (self.dims - 2))
-        else:
-            return self.rope_theta
-
 
     def __call__(
         self,
         x: mx.array,
         mask: Optional[mx.array] = None,
         cache: Optional[Tuple[mx.array, mx.array]] = None,
-        ) -> mx.array:
-            B, L, D = x.shape
-
-            dynamic_base = self.compute_dynamic_base(L)
-            self.rope.base = dynamic_base
+        ):
+            B, L, _ = x.shape
 
             queries, keys, values = self.q_proj(x), self.k_proj(x), self.v_proj(x)
 
-            queries = queries.reshape(B, L, self.num_heads, self.head_dim).transpose(0, 2, 1, 3)
-            keys = keys.reshape(B, L, self.num_key_value_heads, self.head_dim).transpose(0, 2, 1, 3)
-            values = values.reshape(B, L, self.num_key_value_heads, self.head_dim).transpose(0, 2, 1, 3)
+            queries = queries.reshape(B, L, self.num_heads, -1).transpose(0, 2, 1, 3)
+            keys = keys.reshape(B, L, self.num_key_value_heads, -1).transpose(0, 2, 1, 3)
+            values = values.reshape(B, L, self.num_key_value_heads, -1).transpose(0, 2, 1, 3)
 
             if cache is not None:
                 key_cache, value_cache = cache
@@ -133,7 +110,7 @@ class Attention(nn.Module):
                 queries, keys, values, scale=self.scale, mask=mask
             )
 
-            attn_output = attn_output.transpose(0, 2, 1, 3).reshape(B, L, self.hidden_size)
+            attn_output = attn_output.transpose(0, 2, 1, 3).reshape(B, L, -1)
 
             return self.o_proj(attn_output), (keys, values)
 
@@ -147,8 +124,8 @@ class DecoderLayer(nn.Module):
 
         self.self_attn = Attention(args)
         self.mlp = MLP(args)
-        self.input_layernorm = MiniCPMRMSNorm(args.hidden_size, eps=args.rms_norm_eps)
-        self.post_attention_layernorm = MiniCPMRMSNorm(args.hidden_size, eps=args.rms_norm_eps)
+        self.input_layernorm = nn.RMSNorm(args.hidden_size, eps=args.rms_norm_eps)
+        self.post_attention_layernorm = nn.RMSNorm(args.hidden_size, eps=args.rms_norm_eps)
 
         self.scale_depth = args.scale_depth
         self.num_hidden_layers = args.num_hidden_layers
@@ -228,6 +205,7 @@ class Model(nn.Module):
         if "lm_head.weight" not in weights:
             weights["lm_head.weight"] = weights["model.embed_tokens.weight"]
         return weights
+
 
     @property
     def layers(self):
