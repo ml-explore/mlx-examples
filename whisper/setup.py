6775d6cb3f9426c13219ccd0dd854bbe899630a9
madroid <madroidmaq@gmail.com>
2024-05-02 00:00:02 +0800

    Whisper: Add pip distribution configuration to support pip installations. (#739)
    * Whisper: rename whisper to mlx_whisper
    
    * Whisper: add setup.py config for publish
    
    * Whisper: add assets data to setup config
    
    * Whisper: pre-commit for setup.py
    
    * Whisper: Update README.md
    
    * Whisper: Update README.md
    
    * nits
    
    * fix package data
    
    * nit in readme
    
    ---------
    
    Co-authored-by: Awni Hannun <awni@apple.com>

--- whisper/setup.py
+++ whisper/setup.py
@@ -3,19 +3,21 @@
 import sys
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
-package_dir = Path(__file__).parent / "mlx_lm"
+package_dir = Path(__file__).parent / "mlx_whisper"
+
 with open(package_dir / "requirements.txt") as fid:
     requirements = [l.strip() for l in fid.readlines()]
 
 sys.path.append(str(package_dir))
+
 from version import __version__
 
 setup(
-    name="mlx-lm",
+    name="mlx-whisper",
     version=__version__,
-    description="LLMs on Apple silicon with MLX and the Hugging Face Hub",
+    description="OpenAI Whisper on Apple silicon with MLX and the Hugging Face Hub",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     readme="README.md",
@@ -24,16 +26,7 @@ setup(
     url="https://github.com/ml-explore/mlx-examples",
     license="MIT",
     install_requires=requirements,
-    packages=["mlx_lm", "mlx_lm.models", "mlx_lm.tuner"],
+    packages=find_packages(),
+    include_package_data=True,
     python_requires=">=3.8",
-    entry_points={
-        "console_scripts": [
-            "mlx_lm.convert = mlx_lm.convert:main",
-            "mlx_lm.fuse = mlx_lm.fuse:main",
-            "mlx_lm.generate = mlx_lm.generate:main",
-            "mlx_lm.lora = mlx_lm.lora:main",
-            "mlx_lm.merge = mlx_lm.merge:main",
-            "mlx_lm.server = mlx_lm.server:main",
-        ]
-    },
 )
