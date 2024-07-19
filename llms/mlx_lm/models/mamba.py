abab1d0969faabbacc8b0163aeb89545989290e4
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-19 22:42:29 +0200

    adding x, and dt projection layers

--- llms/mlx_lm/models/mamba.py
+++ llms/mlx_lm/models/mamba.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Dict, Optional, Tuple, Union
 
+import math
+
 import mlx.core as mx
 import mlx.nn as nn
 
@@ -10,6 +12,7 @@ from base import BaseModelArgs, KVCache, create_additive_causal_mask
 @dataclass
 class ModelArgs(BaseModelArgs):
     model_type: str = "mamba"
+    dt_rank: Union[int, str] = "auto"
     d_model: int = 12 # hidden_size
     d_inner: int = 2
     vocab_size: int = 623
@@ -18,6 +21,14 @@ class ModelArgs(BaseModelArgs):
     use_bias: bool = False
     use_conv_bias: bool = False
     conv_kernel: int = 4
+    state_size: int = 16
+    expand: int = 2
+
+    def __post_init__(self):
+        self.d_inner = self.expand * self.d_model
+
+        if self.dt_rank == 'auto':
+            self.dt_rank = math.ceil(self.d_model / 16)
 
 
 class DepthWiseConv1d(nn.Module):
@@ -30,6 +41,10 @@ class MambaBlock(nn.Module):
         super().__init__()
         self.in_proj = nn.Linear(args.d_model, 2 * args.d_inner, bias=args.use_bias)
         self.conv1d = DepthWiseConv1d(channels=args.d_inner, kernel_size=args.conv_kernel, bias=args.use_conv_bias, padding=args.conv_kernel-1)
+        self.x_proj = nn.Linear(args.d_inner, args.dt_rank + 2 * args.state_size, bias=False)
+        self.dt_proj = nn.Linear(args.dt_rank, args.d_inner, bias=True)
+
+        dt_init_std = args.dt_rank**-0.5 * args.state_size
 
 
 class ResidualBlock(nn.Module):
