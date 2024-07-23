3d3d17663a52ccedff05dbe534fb499aea22dfab
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-23 12:02:06 +0200

    fix

--- llms/mlx_lm/tuner/trainer.py
+++ llms/mlx_lm/tuner/trainer.py
@@ -293,13 +293,14 @@ def train(
         # Save adapter weights
         if it % args.steps_per_save == 0:
             save_adapter_model(model, args.adapter_file)
+
             if args.fine_tune_type == "full":
                 checkpoint = (
-                    Path(args.adapter_file).parent / f"{it:07d}_model.safetensors"
+                    Path(args.adapter_file).parent / "checkpoints" / f"{it:07d}_model.safetensors"
                 )
                 save_adapter_model(model, checkpoint)
                 print(
-                    f"Iter {it}: Saved adapter weights to "
+                    f"Iter {it}: Saved model checkpoint weights to "
                     f"{args.adapter_file} and {checkpoint}."
                 )
             else:
@@ -316,7 +317,7 @@ def train(
     if args.fine_tune_type == "full":
         full_model_path = Path(args.adapter_file)
         full_model_path.parent.mkdir(parents=True, exist_ok=True)
-        # save_weights(full_model_path, weights=model)
+        # save final full model weights
         save_adapter_model(model, args.adapter_file)
         print(f"Saved final full model weights to {full_model_path}.")
     else:
