259b9ba06f22202b804f7c54f01911ca2b94e1d7
Alex Cheema <alexcheema123@gmail.com>
2024-07-23 12:17:00 -0700

    TODO for rope eval

--- llms/mlx_lm/models/llama.py
+++ llms/mlx_lm/models/llama.py
@@ -103,6 +103,7 @@ class DynamicNTKScalingRoPE(nn.Module):
                 )
                 new_base_freqs.append(freq * ((1 - smooth) * factor + smooth))
 
+        # TODO: It's not so great to have a graph evaluation at each call to RopE (e.g. item). That will be slow.
         return mx.array(new_base_freqs).mean().item()
 
     def extra_repr(self):
