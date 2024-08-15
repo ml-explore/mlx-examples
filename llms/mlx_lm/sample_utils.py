d9e2e839f97b0371b82298fd3eaf23a880506b09
Awni Hannun <awni@apple.com>
2024-08-15 15:31:25 -0700

    nits

--- llms/mlx_lm/sample_utils.py
+++ llms/mlx_lm/sample_utils.py
@@ -7,20 +7,25 @@ import mlx.core as mx
 
 @partial(mx.compile, inputs=mx.random.state, outputs=mx.random.state)
 def min_p_sampling(
-    logits: mx.array, min_p: float, min_tokens_to_keep: int = 1
+    logits: mx.array,
+    min_p: float,
+    min_tokens_to_keep: int = 1,
+    temperature=1.0,
 ) -> mx.array:
     """
-    Performs min-p, i.e. keeps all tokens that are above a minimum probability, scaled by the
-    probability of the most likely token. As a result, the filter becomes more aggressive in the presence of
-    high-probability tokens, which is a sign of a confident output that we shouldn't deviate from.
+    Apply min-p sampling to the logits.
+
+    Min-p keeps all tokens that are above a minimum probability, scaled by the
+    probability of the most likely token. As a result, the filter is more
+    aggressive given a very high-probability token.
+
     Args:
         logits: The logits from the model's output.
-        min_p (`float`):
-            Minimum token probability, which will be scaled by the probability of the most likely token. It must be a
-            value between 0 and 1. Typical values are in the 0.01-0.2 range, comparably selective as setting `top_p` in
-            the 0.99-0.8 range (use the opposite of normal `top_p` values).
-        min_tokens_to_keep (`int`, *optional*, defaults to 1):
-            Minimum number of tokens that cannot be filtered.
+        min_p (float): Minimum token probability. Typical values are in the
+            0.01-0.2 range, comparably selective as setting `top_p` in the
+            0.99-0.8 range.
+        min_tokens_to_keep (int, optional): Minimum number of tokens that cannot
+            be filtered. Default: ``1``.
 
     """
     if not (0 <= min_p <= 1.0):
@@ -34,36 +39,28 @@ def min_p_sampling(
     # reference implementation: https://github.com/huggingface/transformers/blob/main/src/transformers/generation/logits_process.py#L531-L605
 
     # Softmax probabilities
-    probs = mx.softmax(logits, axis=-1)
+    probs = mx.softmax(logits * (1 / temperature), axis=-1)
 
     # Indices sorted in decreasing order
-    sorted_indices = mx.argsort(-logits)
+    sorted_indices = mx.argsort(-logits).squeeze(0)
+    sorted_probs = probs[..., sorted_indices]
 
-    sorted_probs = probs[..., sorted_indices.squeeze(0)]
     # Top probability
-    top_probs = probs.squeeze(0)[sorted_indices.squeeze(0)[0]]
+    top_probs = probs[..., sorted_indices[0]]
 
-    # Calculate the actual min_p threshold by scaling min_p with the top token's probability
+    # Calculate the min_p threshold
     scaled_min_p = min_p * top_probs
 
-    # Create a mask for tokens that have a probability less than the scaled min_p
-    tokens_to_remove = probs < scaled_min_p
-
-    # Mask in sorted order (ensuring minimal tokens are kept)
-    sorted_indices_to_remove = mx.take_along_axis(
-        tokens_to_remove, sorted_indices, axis=1
-    )
-    sorted_indices_to_remove[..., :min_tokens_to_keep] = False
+    # Mask tokens that have a probability less than the scaled min_p
+    tokens_to_remove = sorted_probs < scaled_min_p
+    tokens_to_remove[..., :min_tokens_to_keep] = False
 
     # Create pool of tokens with probability less than scaled min_p
-    selected_probs = mx.where(
-        sorted_indices_to_remove, mx.zeros_like(sorted_probs), sorted_probs
-    )
+    selected_probs = mx.where(tokens_to_remove, 0, sorted_probs)
 
     # Return sampled token
     sorted_token = mx.random.categorical(mx.log(selected_probs))
-    token = sorted_indices.squeeze(0)[sorted_token]
-    return token
+    return sorted_indices[sorted_token]
 
 
 @partial(mx.compile, inputs=mx.random.state, outputs=mx.random.state)
