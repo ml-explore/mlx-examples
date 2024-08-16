cec7df2c1e6022335a6366f732f4f3e707007c07
Awni Hannun <awni@apple.com>
2024-08-16 10:08:54 -0700

    nits

--- whisper/mlx_whisper/cli.py
+++ whisper/mlx_whisper/cli.py
@@ -1,3 +1,5 @@
+# Copyright © 2024 Apple Inc.
+
 import argparse
 import os
 import traceback
@@ -5,49 +7,62 @@ import warnings
 
 from .tokenizer import LANGUAGES, TO_LANGUAGE_CODE
 from .transcribe import transcribe
-from .utils import get_writer, optional_float, optional_int, str2bool
+from .writers import get_writer
 
 
 def build_parser():
+    def optional_int(string):
+        return None if string == "None" else int(string)
+
+    def optional_float(string):
+        return None if string == "None" else float(string)
+
+    def str2bool(string):
+        str2val = {"True": True, "False": False}
+        if string in str2val:
+            return str2val[string]
+        else:
+            raise ValueError(f"Expected one of {set(str2val.keys())}, got {string}")
+
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        "audio", nargs="+", type=str, help="audio file(s) to transcribe"
+        "audio", nargs="+", type=str, help="Audio file(s) to transcribe"
     )
     parser.add_argument(
         "--model",
         default="mlx-community/whisper-tiny",
         type=str,
-        help="the path to save model files, or the hugging face repo id to use",
+        help="The model directory or hugging face repo",
     )
     parser.add_argument(
         "--output-dir",
         "-o",
         type=str,
         default=".",
-        help="directory to save the outputs",
+        help="Directory to save the outputs",
     )
     parser.add_argument(
         "--output-format",
         "-f",
         type=str,
-        default="all",
+        default="txt",
         choices=["txt", "vtt", "srt", "tsv", "json", "all"],
-        help="format of the output file; if not specified, all available formats will be produced",
+        help="Format of the output file",
     )
     parser.add_argument(
         "--verbose",
         type=str2bool,
         default=True,
-        help="whether to print out the progress and debug messages",
+        help="Whether to print out progress and debug messages",
     )
     parser.add_argument(
         "--task",
         type=str,
         default="transcribe",
         choices=["transcribe", "translate"],
-        help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')",
+        help="Perform speech recognition ('transcribe') or speech translation ('translate')",
     )
     parser.add_argument(
         "--language",
@@ -55,52 +70,52 @@ def build_parser():
         default=None,
         choices=sorted(LANGUAGES.keys())
         + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]),
-        help="language spoken in the audio, specify None to perform language detection",
+        help="Language spoken in the audio, specify None to auto-detect",
     )
     parser.add_argument(
-        "--temperature", type=float, default=0, help="temperature to use for sampling"
+        "--temperature", type=float, default=0, help="Temperature for sampling"
     )
     parser.add_argument(
         "--best-of",
         type=optional_int,
         default=5,
-        help="number of candidates when sampling with non-zero temperature",
+        help="Number of candidates when sampling with non-zero temperature",
     )
     parser.add_argument(
         "--patience",
         type=float,
         default=None,
-        help="optional patience value to use in beam decoding, as in https://arxiv.org/abs/2204.05424, the default (1.0) is equivalent to conventional beam search",
+        help="Optional patience value to use in beam decoding, as in https://arxiv.org/abs/2204.05424, the default (1.0) is equivalent to conventional beam search",
     )
     parser.add_argument(
         "--length-penalty",
         type=float,
         default=None,
-        help="optional token length penalty coefficient (alpha) as in https://arxiv.org/abs/1609.08144, uses simple length normalization by default",
+        help="Optional token length penalty coefficient (alpha) as in https://arxiv.org/abs/1609.08144, uses simple length normalization by default.",
     )
     parser.add_argument(
         "--suppress-tokens",
         type=str,
         default="-1",
-        help="comma-separated list of token ids to suppress during sampling; '-1' will suppress most special characters except common punctuations",
+        help="Comma-separated list of token ids to suppress during sampling; '-1' will suppress most special characters except common punctuations",
     )
     parser.add_argument(
         "--initial-prompt",
         type=str,
         default=None,
-        help="optional text to provide as a prompt for the first window.",
+        help="Optional text to provide as a prompt for the first window.",
     )
     parser.add_argument(
         "--condition-on-previous-text",
         type=str2bool,
         default=True,
-        help="if True, provide the previous output of the model as a prompt for the next window; disabling may make the text inconsistent across windows, but the model becomes less prone to getting stuck in a failure loop",
+        help="If True, provide the previous output of the model as a prompt for the next window; disabling may make the text inconsistent across windows, but the model becomes less prone to getting stuck in a failure loop",
     )
     parser.add_argument(
         "--fp16",
         type=str2bool,
         default=True,
-        help="whether to perform inference in fp16; True by default",
+        help="Whether to perform inference in fp16",
     )
     parser.add_argument(
         "--compression-ratio-threshold",
@@ -112,31 +127,31 @@ def build_parser():
         "--logprob-threshold",
         type=optional_float,
         default=-1.0,
-        help="if the average log probability is lower than this value, treat the decoding as failed",
+        help="If the average log probability is lower than this value, treat the decoding as failed",
     )
     parser.add_argument(
         "--no-speech-threshold",
         type=optional_float,
         default=0.6,
-        help="if the probability of the  token is higher than this value AND the decoding has failed due to `logprob_threshold`, consider the segment as silence",
+        help="If the probability of the token is higher than this value the decoding has failed due to `logprob_threshold`, consider the segment as silence",
     )
     parser.add_argument(
         "--word-timestamps",
         type=str2bool,
         default=False,
-        help="(experimental) extract word-level timestamps and refine the results based on them",
+        help="Extract word-level timestamps and refine the results based on them",
     )
     parser.add_argument(
         "--prepend-punctuations",
         type=str,
         default="\"'“¿([{-",
-        help="if word_timestamps is True, merge these punctuation symbols with the next word",
+        help="If word-timestamps is True, merge these punctuation symbols with the next word",
     )
     parser.add_argument(
         "--append-punctuations",
         type=str,
         default="\"'.。,，!！?？:：”)]}、",
-        help="if word_timestamps is True, merge these punctuation symbols with the previous word",
+        help="If word_timestamps is True, merge these punctuation symbols with the previous word",
     )
     parser.add_argument(
         "--highlight-words",
@@ -163,27 +178,22 @@ def build_parser():
         help="(requires --word_timestamps True, no effect with --max_line_width) the maximum number of words in a segment",
     )
     parser.add_argument(
-        "--clip-timestamps",
-        type=str,
-        default="0",
-        help="comma-separated list start,end,start,end,... timestamps (in seconds) of clips to process, where the last end timestamp defaults to the end of the file",
-    )
-    parser.add_argument(
         "--hallucination-silence-threshold",
         type=optional_float,
         help="(requires --word_timestamps True) skip silent periods longer than this threshold (in seconds) when a possible hallucination is detected",
+    )
+    parser.add_argument(
+        "--clip-timestamps",
+        type=str,
+        default="0",
+        help="Comma-separated list start,end,start,end,... timestamps (in seconds) of clips to process, where the last end timestamp defaults to the end of the file",
     )
     return parser
 
 
 def main():
     parser = build_parser()
-    args = parser.parse_args().__dict__
-    print(f"Raw Args: {args}")
-    print(f"-----")
-    # Convert hyphenated args to underscore
-    args = {k.replace("-", "_"): v for k, v in args.items()}
-
+    args = vars(parser.parse_args())
     if args["verbose"] is True:
         print(f"Args: {args}")
 
@@ -192,8 +202,6 @@ def main():
     output_format: str = args.pop("output_format")
     os.makedirs(output_dir, exist_ok=True)
 
-    temperature = args.pop("temperature")
-
     writer = get_writer(output_format, output_dir)
     word_options = [
         "highlight_words",
@@ -201,21 +209,21 @@ def main():
         "max_line_width",
         "max_words_per_line",
     ]
-    if not args["word_timestamps"]:
-        for option in word_options:
-            if args[option]:
-                parser.error(f"--{option} requires --word_timestamps True")
-    if args["max_line_count"] and not args["max_line_width"]:
-        warnings.warn("--max_line_count has no effect without --max_line_width")
-    if args["max_words_per_line"] and args["max_line_width"]:
-        warnings.warn("--max_words_per_line has no effect with --max_line_width")
     writer_args = {arg: args.pop(arg) for arg in word_options}
+    if not args["word_timestamps"]:
+        for k, v in writer_args.items():
+            if v:
+                argop = k.replace("_", "-")
+                parser.error(f"--{argop} requires --word-timestamps True")
+    if writer_args["max_line_count"] and not writer_args["max_line_width"]:
+        warnings.warn("--max-line-count has no effect without --max-line-width")
+    if writer_args["max_words_per_line"] and writer_args["max_line_width"]:
+        warnings.warn("--max-words-per-line has no effect with --max-line-width")
     for audio_path in args.pop("audio"):
         try:
             result = transcribe(
                 audio_path,
                 path_or_hf_repo=path_or_hf_repo,
-                temperature=temperature,
                 **args,
             )
             writer(result, audio_path, **writer_args)
--- whisper/mlx_whisper/writers.py
+++ whisper/mlx_whisper/writers.py
@@ -1,50 +1,11 @@
+# Copyright © 2024 Apple Inc.
+
 import json
 import os
 import re
 import sys
 import zlib
 from typing import Callable, List, Optional, TextIO
-
-system_encoding = sys.getdefaultencoding()
-
-if system_encoding != "utf-8":
-
-    def make_safe(string):
-        # replaces any character not representable using the system default encoding with an '?',
-        # avoiding UnicodeEncodeError (https://github.com/openai/whisper/discussions/729).
-        return string.encode(system_encoding, errors="replace").decode(system_encoding)
-
-else:
-
-    def make_safe(string):
-        # utf-8 can encode any Unicode code point, so no need to do the round-trip encoding
-        return string
-
-
-def exact_div(x, y):
-    assert x % y == 0
-    return x // y
-
-
-def str2bool(string):
-    str2val = {"True": True, "False": False}
-    if string in str2val:
-        return str2val[string]
-    else:
-        raise ValueError(f"Expected one of {set(str2val.keys())}, got {string}")
-
-
-def optional_int(string):
-    return None if string == "None" else int(string)
-
-
-def optional_float(string):
-    return None if string == "None" else float(string)
-
-
-def compression_ratio(text) -> float:
-    text_bytes = text.encode("utf-8")
-    return len(text_bytes) / len(zlib.compress(text_bytes))
 
 
 def format_timestamp(
@@ -72,13 +33,6 @@ def get_start(segments: List[dict]) -> Optional[float]:
     return next(
         (w["start"] for s in segments for w in s["words"]),
         segments[0]["start"] if segments else None,
-    )
-
-
-def get_end(segments: List[dict]) -> Optional[float]:
-    return next(
-        (w["end"] for s in reversed(segments) for w in reversed(s["words"])),
-        segments[-1]["end"] if segments else None,
     )
 
 
