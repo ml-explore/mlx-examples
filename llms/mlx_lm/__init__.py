db7d3c7a7618cc99f84f06f19c71e55d6ec59033
Awni Hannun <awni@apple.com>
2024-10-26 18:11:14 -0700

    remove transformer flax/torch warning

--- llms/mlx_lm/__init__.py
+++ llms/mlx_lm/__init__.py
@@ -1,4 +1,10 @@
 # Copyright © 2023-2024 Apple Inc.
 
 from ._version import __version__
+
+import os
+os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
+
 from .utils import convert, generate, load, stream_generate
+
+
