6d4290928fbfdf2871e51c20c0b969edb3a3e561
Awni Hannun <awni@apple.com>
2024-10-26 18:14:56 -0700

    format

--- llms/mlx_lm/__init__.py
+++ llms/mlx_lm/__init__.py
@@ -1,10 +1,9 @@
 # Copyright © 2023-2024 Apple Inc.
 
+import os
+
 from ._version import __version__
 
-import os
 os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
 
 from .utils import convert, generate, load, stream_generate
-
-
