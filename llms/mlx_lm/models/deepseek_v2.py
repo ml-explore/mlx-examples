361e3547e818117ce9ad61e118282dfc2efba373
Awni Hannun <awni@apple.com>
2025-02-11 13:41:36 -0800

    fix sharding for more even number of layers

--- llms/mlx_lm/models/deepseek_v2.py
+++ llms/mlx_lm/models/deepseek_v2.py
@@ -378,9 +378,11 @@ class DeepseekV2Model(nn.Module):
         # rank=pipeline_size-1 gets the first
         self.pipeline_rank = group.rank()
         self.pipeline_size = group.size()
-        layers_per_rank = (
-            len(self.layers) + self.pipeline_size - 1
-        ) // self.pipeline_size
+        layers_per_rank = len(self.layers) // self.pipeline_size
+        extra = len(self.layers) - layers_per_rank * self.pipeline_size
+        if self.pipeline_rank < extra:
+            layers_per_rank += 1
+
         self.start_idx = (self.pipeline_size - self.pipeline_rank - 1) * layers_per_rank
         self.end_idx = self.start_idx + layers_per_rank
         self.num_layers = layers_per_rank
