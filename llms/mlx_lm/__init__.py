0ac7c0330a2ab8139b4137c3b1929fefd32ea313
Awni Hannun <awni@apple.com>
2025-03-18 13:00:28 -0700

    remove mlx lm

--- llms/mlx_lm/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright © 2023-2024 Apple Inc.
-
-import os
-
-from ._version import __version__
-
-os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
-
-from .utils import convert, generate, load, stream_generate
